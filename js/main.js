054d740d07692aeb731f52d38f31d1af9588bf46
ivanpilipchack <pilipchackiivan@gmail.com>
2018-10-21 17:23:27 +0200

    full project

--- js/main.js
+++ js/main.js
@@ -92,15 +92,23 @@ $(document).ready(function() {
             });
         })
         //end
-    $(".button").click(function() {
-        $(".hidden").fadeToggle(2000);
-    });
+    var $readMore = $('.button');
+    var $openMore = $('.more');
+    $readMore.on('click', () => {
+        $openMore.slideToggle(() => {
+            $readMore.toggleClass('addButton');
+        });
+    })
 
 
     //animate
 
     $(".info_btn").click(function(event) {
-        $(event.currentTarget).siblings('.plans_info').slideToggle(1000, 'linear', function() {
+        $(event.currentTarget).siblings('.plans_info').slideDown(1000, 'linear', function() {
+            $(event.currentTarget).siblings('.conect_plans')
+                .addClass('addBorder')
+                .html('<img class="good" src="images/good.png">');
+            //$('.good').addClass('addRotate');
             $(event.currentTarget).siblings('.close').delay(500).show();
             $(this).addClass('click');
             $(this).next().addClass('click').text('Open');
@@ -108,11 +116,103 @@ $(document).ready(function() {
     });
     const $closeOrder = $('.close');
     const $btnOpen = $('.info_btn');
-    $closeOrder.on('click', () => {
+
+
+
+    $closeOrder.on('click', (event) => {
         $(event.currentTarget).addClass('closeOut').fadeOut(500);
+        $(event.currentTarget).siblings('.conect_plans').removeClass('addBorder');
         $btnOpen.text('Order Now');
         $(event.currentTarget).siblings('.plans_info').fadeOut(800);
+    });
+    const $first = $('.first');
+    const $left = $('.left');
+    const $right = $('.right');
+    const $last = $('.last')
+    $first.on('click', () => {
+        $('.first_number').html('<h3 class="plans">20/m</h3>')
+    });
+    $left.on('click', () => {
+        $('.number_center').html('<h3 class="plans">40/m</h3>')
+    });
+    $right.on('click', () => {
+        $('.number_right').html('<h3 class="plans">80/m</h3>')
+    });
+    $last.on('click', () => {
+        $('.number_last').html('<h3 class="plans">120/m</h3>')
+    });
+    //select menu
+    const $basic = $('.basic');
+    const $standart = $('.standart');
+    const $business = $('.business');
+    const $premium = $('.premium');
+
+    $basic.on('click', () => {
+        $('.first_number').addClass('addSelect');
+    });
+    $standart.on('click', () => {
+        $('.first_number').removeClass('addSelect');
+        $('.number_center').addClass('addSelect');
+    });
+    $business.on('click', () => {
+        $('.number_center').removeClass('addSelect');
+        $('.number_right').addClass('addSelect');
+    });
+    $premium.on('click', () => {
+        $('.number_right').removeClass('addSelect');
+        $('.number_last').addClass('addSelect');
+    });
+    $basic.on('click', () => {
+        $('.start').animate({
+            marginTop: '3px',
+            fontSize: '46px'
+        });
+        $basic.on('mouseleave', () => {
+            $('.start').animate({
+                marginTop: '22px',
+                fontSize: '36px'
+            })
+        })
     })
+    $standart.on('click', () => {
+        $('.min').animate({
+            marginTop: '3px',
+            fontSize: '46px'
+        });
+        $standart.on('mouseleave', () => {
+            $('.min').animate({
+                marginTop: '22px',
+                fontSize: '36px'
+            })
+        })
+    })
+    $business.on('click', () => {
+        $('.max').animate({
+            marginTop: '3px',
+            fontSize: '46px'
+        });
+        $business.on('mouseleave', () => {
+            $('.max').animate({
+                marginTop: '22px',
+                fontSize: '36px'
+            })
+        })
+    })
+    $premium.on('click', () => {
+        $('.end').animate({
+            marginTop: '3px',
+            fontSize: '46px'
+        });
+        $premium.on('mouseleave', () => {
+            $('.end').animate({
+                marginTop: '22px',
+                fontSize: '36px'
+            })
+        })
+    })
+
+
+    //end
 
     $('.form_button').mouseenter(function() {
         $('.form_area').slideToggle(2000);
